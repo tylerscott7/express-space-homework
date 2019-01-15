2db5bd8ae0c2361576f5d2229962f1a5223d2fa9
Tyler Nicholson <tylernicholson@Tylers-MacBook-Pro.local>
2019-01-15 08:15:24 -0700

    working with express and ejs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require('express');
 const app = express();
+const Missions = require('./models/marsMissions');
 
 // run `npm install` to install dependencies in package.json
 
@@ -22,53 +23,63 @@ const port = 3000;
 
 // DATA - put into marsMissions.js file inside of a models folder, for module.exports
 // remember to require it in the server
-const marsMissions = [
-  {
-    name: "Curiosity",
-    launchDate: "26 Nov 2011",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Opportunity",
-    launchDate: "8 Jul 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Spirit",
-    launchDate: "10 Jun 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Sojourner",
-    launchDate: "4 Dec 1996",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Rosetta",
-    launchDate: "2 Mar 2004",
-    operator: "ESA",
-    missionType: "Gravity Assist",
-    img: ""
-  }
-];
+// const marsMissions = [
+//   {
+//     name: "Curiosity",
+//     launchDate: "26 Nov 2011",
+//     operator: "NASA",
+//     missionType: "Rover",
+//     img: ""
+//   },
+//   {
+//     name: "Opportunity",
+//     launchDate: "8 Jul 2003",
+//     operator: "NASA",
+//     missionType: "Rover",
+//     img: ""
+//   },
+//   {
+//     name: "Spirit",
+//     launchDate: "10 Jun 2003",
+//     operator: "NASA",
+//     missionType: "Rover",
+//     img: ""
+//   },
+//   {
+//     name: "Sojourner",
+//     launchDate: "4 Dec 1996",
+//     operator: "NASA",
+//     missionType: "Rover",
+//     img: ""
+//   },
+//   {
+//     name: "Rosetta",
+//     launchDate: "2 Mar 2004",
+//     operator: "ESA",
+//     missionType: "Gravity Assist",
+//     img: ""
+//   }
+// ];
 
 // INDEX Route
 // send data to 'missions/index.ejs' view
 // the view should display just the names of each mission
 // display the mission names as <li> in a <ul> with the class name "missions"
+app.get('/index', (req, res) => {
+  res.render('missions/index.ejs', {
+    mission: Missions,
+  });
+});
+
 
 // SHOW Route
 // send data to 'missions/show.ejs' view
 // the view should display all the data for a single mission
-
+app.get('/index/:index', (req, res) => {
+  res.render('missions/show.ejs', {
+    missions: Missions[req.params.index],
+  });
+});
 
 
 // LISTENER
